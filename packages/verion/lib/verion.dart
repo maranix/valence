6d20f701ed8812f297b8c22de2c3f0dd3f2fcc27
maranix <ramanverma4183@gmail.com>
2026-04-11 13:56:10 +0530

    feat(batch): make batch public

--- packages/verion/lib/verion.dart
+++ packages/verion/lib/verion.dart
@@ -4,6 +4,7 @@ export 'src/core/source.dart' show Source, source, SourceEvent;
 export 'src/core/derive.dart' show Derive, derive;
 export 'src/core/observe.dart' show Observe, observe;
 export 'src/core/scope.dart' show VerionScope, createScope;
+export 'src/core/batch.dart' show batch;
 
 export 'src/types.dart';
 export 'src/observer.dart';
