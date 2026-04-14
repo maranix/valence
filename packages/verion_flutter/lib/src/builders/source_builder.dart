be987e23be571583cd9bd80efa778ef67bd73de3
maranix <ramanverma4183@gmail.com>
2026-04-14 11:24:21 +0530

    feat(verion_flutter): use raw listener in SourceBuilder

--- packages/verion_flutter/lib/src/builders/source_builder.dart
+++ packages/verion_flutter/lib/src/builders/source_builder.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 import 'package:verion/verion.dart';
-import 'package:verion_flutter/src/builders/derive_builder.dart';
 
-class SourceBuilder<T, E extends SourceEvent<T>> extends StatelessWidget {
+class SourceBuilder<T, E extends SourceEvent<T>> extends StatefulWidget {
   const SourceBuilder({
     super.key,
     required this.source,
@@ -14,10 +13,46 @@ class SourceBuilder<T, E extends SourceEvent<T>> extends StatelessWidget {
   final Widget Function(T) builder;
 
   @override
+  State<SourceBuilder<T, E>> createState() => _SourceBuilderState<T, E>();
+}
+
+class _SourceBuilderState<T, E extends SourceEvent<T>>
+    extends State<SourceBuilder<T, E>> {
+  late T _value;
+
+  void _onChangeListener(T val) {
+    setState(() {
+      _value = val;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _value = widget.source.value;
+    widget.source.addListener(_onChangeListener);
+  }
+
+  @override
+  void didUpdateWidget(covariant SourceBuilder<T, E> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.source != widget.source) {
+      oldWidget.source.removeListener(_onChangeListener);
+      _value = widget.source.value;
+      widget.source.addListener(_onChangeListener);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.source.removeListener(_onChangeListener);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DeriveBuilder<T>(
-      derive: (sub) => sub(source),
-      builder: builder,
-    );
+    return widget.builder(_value);
   }
 }
